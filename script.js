d865ee818ccb7f58997419d3a8cd7a4ffdf29d3e
ixenbrandonestiben-lang <ixenbrandonestiben@gmail.com>
2026-05-06 12:53:23 -0600

    feat: :construction: manejo de errore js
    manejo de errore js

--- script.js
+++ script.js
@@ -22,7 +22,13 @@ fetch(url)
         const name = `${user.name.first} ${user.name.last}`;
         const email = user.email;
         const picture = user.picture.large;
-
+        const cell = user.cell;
+        const city = user.location.city;
+        const country = user.location.country;
+        const state = user.location.state;
+        const street = `${user.location.street.number} ${user.location.street.name}`;
+        const postcode = user.location.postcode;
+        
         const div_principal1 = document.createElement("div");
         const nameElement1 = document.createElement("p");
         nameElement1.textContent = name;
@@ -44,10 +50,65 @@ fetch(url)
         nameElement3.textContent = picture;
         nameElement3.className = "picture";
         nameElement3.src = picture;
-        
-    
+        nameElement3.alt = "User Picture";
+
         div_principal3.appendChild(nameElement3);
         document.body.appendChild(div_principal3);
+    
+        
+        const div_principal4 = document.createElement("div");
+        const nameElement4 = document.createElement("p");
+        nameElement4.textContent = cell;
+        nameElement4.className = "cell";
+    
+        div_principal4.appendChild(nameElement4);
+        document.body.appendChild(div_principal4);
+    
       
+        const div_principal5 = document.createElement("div");
+        const nameElement5 = document.createElement("p");
+        nameElement5.textContent = city;
+        nameElement5.className = "city";
+        
+        
+    
+        div_principal5.appendChild(nameElement5);
+        document.body.appendChild(div_principal5);
+
+        const div_principal6 = document.createElement("div");
+        const nameElement6 = document.createElement("p");
+        nameElement6.textContent = country;
+        nameElement6.className = "country";
+        
+    
+        div_principal6.appendChild(nameElement6);
+        document.body.appendChild(div_principal6);
+
+        const div_principal7 = document.createElement("div");
+        const nameElement7 = document.createElement("p");
+        nameElement7.textContent = state;
+        nameElement7.className = "state";
+        
+    
+        div_principal7.appendChild(nameElement7);
+        document.body.appendChild(div_principal7);
+
+        const div_principal8 = document.createElement("div");
+        const nameElement8 = document.createElement("p");
+        nameElement8.textContent = street;
+        nameElement8.className = "street";
+        
+    
+        div_principal8.appendChild(nameElement8);
+        document.body.appendChild(div_principal8);
+
+        const div_principal9 = document.createElement("div");
+        const nameElement9 = document.createElement("p");
+        nameElement9.textContent = postcode;
+        nameElement9.className = "postcode";
+        
+    
+        div_principal9.appendChild(nameElement9);
+        document.body.appendChild(div_principal9);
 
     })
